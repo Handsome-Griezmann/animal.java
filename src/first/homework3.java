f669f9fa5a40a3907e29ed56736942ecfd23be35
Handsome-Griezmann <1061152595@qq.com>
2020-10-31 22:55:40 +0800

    Initial commit

---
+++ src/first/homework3.java
@@ -0,0 +1,17 @@
+package first;
+import java.util.Scanner;
+
+class Input {
+
+public static class homework3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);//定义scanner，等待输入
+        System.out.println("输入数字:");
+        boolean shuzi =in.nextBoolean();
+
+    }
+
+        }
+}
+
+
---
+++ src/qw.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Input {
+    public static class qw {
+        public void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("输入数字");
+            int i = in.nextInt();
+            Boolean b =false;
+            
+                System.out.println(i + "是偶数");
+                System.out.println(i + "是奇数");
+
+        }
+    }
+}
